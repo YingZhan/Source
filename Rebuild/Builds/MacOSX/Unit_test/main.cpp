375266106435178549dedf2e198215d9bb3003f7
Xinquan <royzxq@gmail.com>
2015-06-02 22:58:04 -0400

    delete old test and add new test target in the reverb; add signal generator

---
+++ Rebuild/Builds/MacOSX/Unit_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Unit_test
+//
+//  Created by XinquanZhou on 6/2/15.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
